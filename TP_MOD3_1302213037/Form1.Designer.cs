617e47777001ced05e85056e092b10ac9497532c
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-03-06 00:20:19 +0800

    Commit GUI FINAL

--- TP_MOD3_1302213037/Form1.Designer.cs
+++ TP_MOD3_1302213037/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            TxtInput = new Label();
+            Output = new Label();
+            buttonBindingSource = new BindingSource(components);
+            SubmitButton = new Button();
+            textBoxBindingSource = new BindingSource(components);
+            InputBox = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)buttonBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBoxBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // TxtInput
+            // 
+            TxtInput.AutoSize = true;
+            TxtInput.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            TxtInput.Location = new Point(42, 87);
+            TxtInput.Name = "TxtInput";
+            TxtInput.Size = new Size(111, 25);
+            TxtInput.TabIndex = 1;
+            TxtInput.Text = "Input Nama";
+            TxtInput.Click += label1_Click_1;
+            // 
+            // Output
+            // 
+            Output.AutoSize = true;
+            Output.DataBindings.Add(new Binding("Name", buttonBindingSource, "DataBindings", true));
+            Output.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            Output.Location = new Point(171, 157);
+            Output.Name = "Output";
+            Output.Size = new Size(0, 30);
+            Output.TabIndex = 3;
+            Output.Click += Output_Click;
+            // 
+            // buttonBindingSource
+            // 
+            buttonBindingSource.DataSource = typeof(Button);
+            // 
+            // SubmitButton
+            // 
+            SubmitButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            SubmitButton.Location = new Point(367, 82);
+            SubmitButton.Name = "SubmitButton";
+            SubmitButton.Size = new Size(104, 35);
+            SubmitButton.TabIndex = 4;
+            SubmitButton.Text = "Submit";
+            SubmitButton.UseVisualStyleBackColor = true;
+            SubmitButton.Click += SubmitButton_Click;
+            // 
+            // textBoxBindingSource
+            // 
+            textBoxBindingSource.DataSource = typeof(TextBox);
+            // 
+            // InputBox
+            // 
+            InputBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            InputBox.ForeColor = SystemColors.InactiveCaption;
+            InputBox.Location = new Point(183, 84);
+            InputBox.Name = "InputBox";
+            InputBox.Size = new Size(154, 32);
+            InputBox.TabIndex = 5;
+            InputBox.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(512, 256);
+            Controls.Add(InputBox);
+            Controls.Add(SubmitButton);
+            Controls.Add(Output);
+            Controls.Add(TxtInput);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)buttonBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBoxBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label TxtInput;
+        private Label Output;
+        private BindingSource buttonBindingSource;
+        private Button SubmitButton;
+        private BindingSource textBoxBindingSource;
+        private TextBox InputBox;
     }
 }
--- TP_MOD3_1302213037/Form1.cs
+++ TP_MOD3_1302213037/Form1.cs
@@ -6,5 +6,32 @@ namespace TP_MOD3_1302213037
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            Output.Text = "Halo, " + InputBox.Text;
+        }
+
+        private void Output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
